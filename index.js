58a9b62f4550aa0537fcd8e17eba6d8b88638ad5
karthik <karthikbhatt22@gmail.com>
2026-08-11 07:46:00 +0530

    feat:folder structure init

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-import http from 'node:http';
-
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ message: 'Hello from Node.js!' }));
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+import './src/server.js';
